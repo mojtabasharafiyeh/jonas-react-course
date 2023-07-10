e933995611bde3e470ff12bcf7afee74cedbd23c
mojtabasharafiyeh <61268429+mojtabasharafiyeh@users.noreply.github.com>
2023-07-10 18:49:51 +0330

    implement v.2

--- src/index.js
+++ src/index.js
@@ -3,33 +3,52 @@ import ReactDOM, { createRoot } from 'react-dom/client'
 
 function Step() {
   const [step, setstep] = useState(1)
-  function upStepHandler() {
-    setstep((cur) => cur + 1)
-  }
-  function downStepHandler() {
-    setstep((cur) => cur - 1)
-  }
+  // function upStepHandler() {
+  //   setstep((cur) => cur + 1)
+  // }
+  // function downStepHandler() {
+  //   setstep((cur) => cur - 1)
+  // }
   const [count, setcount] = useState(0)
-  function upHandler() {
-    setcount((cur) => cur + step)
+  function upHandler(e) {
+    setcount((e) => e + step)
   }
-  function downHandler() {
-    setcount((cur) => cur - step)
+  function downHandler(c) {
+    setcount((c) => c - step)
+  }
+  function rangeHandler(e) {
+    setstep(+e.target.value)
+  }
+  function resetHandler() {
+    setstep(1)
+    setcount(0)
+  }
+  function inputHandler(e) {
+    setcount(+e.target.value)
   }
   return (
     <>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
+      <input
+        type='range'
+        min='0'
+        max='10'
+        value={step}
+        onChange={rangeHandler}
+      ></input>
+      <span>{step}</span>
+      {/* <div style={{ display: 'flex', alignItems: 'center' }}>
         <button onClick={downStepHandler}>-</button>
         <p> step:{step}</p>
         <button onClick={upStepHandler}>+</button>
-      </div>
+      </div> */}
       <div>
         <div style={{ display: 'flex', alignItems: 'center' }}>
-          <button onClick={downHandler}>-</button>
-          <p> count:{count}</p>
-          <button onClick={upHandler}>+</button>
+          <button onClick={() => downHandler(count)}>-</button>
+          <input type='number' value={count} onChange={inputHandler}></input>
+          <button onClick={() => upHandler(count)}>+</button>
         </div>
       </div>
+      <button onClick={resetHandler}>reset</button>
       <div>
         {count < 0 && <span>{-count} dayes ago was </span>}
         {count > 0 && <span>{count} dayes from today </span>}{' '}
