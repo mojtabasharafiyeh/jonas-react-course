525b4b69064a2e0ae75fe834c7f1e9250c92443d
mojtabasharafiyeh <61268429+mojtabasharafiyeh@users.noreply.github.com>
2023-07-24 01:30:54 +0330

    challange 10 thinking in react

--- src/index.js
+++ src/index.js
@@ -1,123 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM, { createRoot } from 'react-dom/client'
 import './index.css'
-const pizzaData = [
-  {
-    name: 'Focaccia',
-    ingredients: 'Bread with italian olive oil and rosemary',
-    price: 6,
-    photoName: 'pizzas/focaccia.jpg',
-    soldOut: false,
-  },
-  {
-    name: 'Pizza Margherita',
-    ingredients: 'Tomato and mozarella',
-    price: 10,
-    photoName: 'pizzas/margherita.jpg',
-    soldOut: false,
-  },
-  {
-    name: 'Pizza Spinaci',
-    ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
-    price: 12,
-    photoName: 'pizzas/spinaci.jpg',
-    soldOut: false,
-  },
-  {
-    name: 'Pizza Funghi',
-    ingredients: 'Tomato, mozarella, mushrooms, and onion',
-    price: 12,
-    photoName: 'pizzas/funghi.jpg',
-    soldOut: false,
-  },
-  {
-    name: 'Pizza Salamino',
-    ingredients: 'Tomato, mozarella, and pepperoni',
-    price: 15,
-    photoName: 'pizzas/salamino.jpg',
-    soldOut: true,
-  },
-  {
-    name: 'Pizza Prosciutto',
-    ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
-    price: 18,
-    photoName: 'pizzas/prosciutto.jpg',
-    soldOut: false,
-  },
-]
-function Menu(props) {
-  const numberPizza = pizzaData.length
+function TextExpander({
+  children,
+  className = 'box',
+  collapsedNumWords = 10,
+  collapseButtonText = 'Collapse text',
+  buttonColor = '#ff6622',
+  expandButtonText = 'Show text',
+  expanded = false,
+}) {
+  console.log(children.length)
+  const [expand, setexpand] = useState(expanded)
+  function handleclick() {
+    setexpand((pre) => !pre)
+  }
   return (
-    <main className='menu'>
-      <h2>Our menu</h2>
-      {numberPizza > 0 ? (
-        <React.Fragment>
-          <p>
-            Authentic Italian cuisine. {numberPizza} creative dishes to choose
-            from. All from our stone oven, all organic, all delicious.
-          </p>
-
-          <ul className='pizzas'>
-            {pizzaData.map((pizzas) => (
-              <Pizza obj={pizzas} />
-            ))}
-          </ul>
-        </React.Fragment>
+    <div className={className}>
+      {expand ? children : children.split(' ', collapsedNumWords).join(' ')}
+      {expand ? (
+        <span style={{ color: buttonColor }} onClick={handleclick}>
+          {collapseButtonText}{' '}
+        </span>
       ) : (
-        <p>We're still working on our menu. Please come back later :)</p>
+        <span onClick={handleclick} style={{ color: buttonColor }}>
+          {expandButtonText}
+        </span>
       )}
-    </main>
-  )
-}
-function Pizza({ obj }) {
-  // if (obj.soldOut) return null
-  return (
-    <li className={`pizza ${obj.soldOut ? 'sold-out' : ''}`}>
-      <img src={obj.photoName} alt={obj.alt}></img>
-      <div>
-        <h3>{obj.name}</h3>
-        <p>{obj.ingredients}</p>
-        <span>{obj.soldOut ? 'sold out' : obj.price}</span>
-      </div>
-    </li>
-  )
-}
-function Header() {
-  // const style={ color: 'red', fontSize: '48px', textTransform: 'uppercase' }
-  const style = {}
-  return (
-    <header className='header'>
-      <h1 style={style}>fast react pizza .co</h1>
-    </header>
-  )
-}
-function Footer() {
-  const hour = new Date().getHours()
-  const openHour = 12
-  const closeHour = 22
-  const isOpen = hour >= openHour && hour <= closeHour
-  console.log(isOpen)
-  return (
-    <div className='footer order'>
-      {' '}
-      {isOpen ? (
-        <footer>
-          We're open from {openHour}:00 to {closeHour}:00. Come visit us or
-          order online.
-        </footer>
-      ) : (
-        <p>we are closed</p>
-      )}
-      <button className='btn'>order now</button>
     </div>
   )
 }
 function App() {
   return (
-    <div className='container'>
-      <Header />
-      <Menu />
-      <Footer />
+    <div>
+      <div>
+        <TextExpander
+          className='box'
+          expandButtonText='Show more'
+          buttonColor='blue'
+          collapseButtonText='show less'
+        >
+          Space travel is the ultimate adventure! Imagine soaring past the stars
+          and exploring new worlds. It's the stuff of dreams and science
+          fiction, but believe it or not, space travel is a real thing. Humans
+          and robots are constantly venturing out into the cosmos to uncover its
+          secrets and push the boundaries of what's possible.
+        </TextExpander>
+
+        <TextExpander
+          collapsedNumWords={20}
+          expandButtonText='Show text'
+          collapseButtonText='Collapse text'
+          buttonColor='#ff6622'
+        >
+          Space travel requires some seriously amazing technology and
+          collaboration between countries, private companies, and international
+          space organizations. And while it's not always easy (or cheap), the
+          results are out of this world. Think about the first time humans
+          stepped foot on the moon or when rovers were sent to roam around on
+          Mars.
+        </TextExpander>
+
+        <TextExpander expanded={true} className='box'>
+          Space missions have given us incredible insights into our universe and
+          have inspired future generations to keep reaching for the stars. Space
+          travel is a pretty cool thing to think about. Who knows what we'll
+          discover next!
+        </TextExpander>
+      </div>
     </div>
   )
 }
